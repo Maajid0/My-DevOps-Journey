0b4a2b9aa33e8d3b5e6dd1dade58b17bf70b0926
Maajid Adde <maajida21@outlook.com>
2025-02-03 19:09:35 +0000

    Create file_ops.sh

---
+++ Bash/Bash Assignment/scripts/file_ops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a directory called Maajid
+mkdir Maajid
+
+# Navigate into the Maajid directory
+cd Maajid
+
+# Write "DevOps is the best" to a file called maajid.txt
+echo "DevOps is the best" > maajid.txt
+
+# Display the contents of maajid.txt
+cat maajid.txt
